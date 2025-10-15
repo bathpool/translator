2b68816dec23edd5319a8301be01c16a1657c9be
bathpool <gujie2000@hotmail.com>
2025-10-15 12:16:30 -0700

    v4 framework doen

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const btn = document.getElementById('btn')
 const textResult = document.getElementById('text-result') 
+const textInput = document.getElementById('textarea')
+let isDefault = true
+textInput.value = ''
 
 btn.addEventListener('click', () => {
-    textResult.classList.toggle('active')
+
+    console.log(textInput.value)
+
+    if ( isDefault && textInput.value ) {
+        togglePage()
+        getTranslation()
+        isDefault = false
+    } else if ( !isDefault) {
+        defaultPage()
+        isDefault = true
+    }
+
 })
 
+function togglePage() {
+    textResult.style.display = 'block'
+
+
+}
+
+function defaultPage() {
+    textResult.style.display = 'none'
+    textInput.value = ''
+}
+
+function getTranslation() {
+    
+
+}
