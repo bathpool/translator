d761709a05eac258050dbc0959ed510aa4dee81c
bathpool <gujie2000@hotmail.com>
2025-11-01 17:25:27 -0700

    v15

--- server.js
+++ server.js
@@ -8,22 +8,7 @@ app.use(express.json());
 app.use('/api', apiRoutes)
 app.use(express.static('public'))
 
-
 app.listen(PORT, ()=> console.log(`server connected on port ${PORT}`))
-
-
-// const server = http.createServer(async (req, res) => {
-//     if (req.url === '/api') {
-//         let body = ''
-//         for await (const chunk of req) {
-//             body += chunk
-//         }
-//         console.log('input received')
-//         await getTranslation(JSON.parse(body), res)
-//     } else {
-//         await serveStatic(req, res, wwwroot)
-//     }
-// })
 
 
 
