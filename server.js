4a73fc4864aa893133d2b2389e2a5f36044ba488
bathpool <gujie2000@hotmail.com>
2025-11-01 19:30:59 -0700

    v32

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { apiRoutes } from '../routes/apiRoutes.js'
+import { apiRoutes } from './routes/apiRoutes.js'
 const PORT = process.env.PORT || 8000
 
 const app = express()
@@ -11,7 +11,4 @@ app.use('/api', apiRoutes)
 
 // app.listen(PORT, ()=> console.log(`server connected on port ${PORT}`))
 
-export default function handler(req, res) {
-  return app(req, res)
-}
-
+export default app
