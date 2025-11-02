6d0f09e7d3cc6d333d6e1fde00883d8c7ca243a3
bathpool <gujie2000@hotmail.com>
2025-11-01 18:59:18 -0700

    v29

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ const PORT = process.env.PORT || 8000
 
 const app = express()
 
-app.use(express.static('public'))
 app.use(express.json());
 app.use('/api', apiRoutes)
 
-app.get('/', (req, res) => {
-    // If index.html exists in public/, Vercel serves that instead
-    res.send('Welcome to the API server'); 
-});
+
 
 // app.listen(PORT, ()=> console.log(`server connected on port ${PORT}`))
 
