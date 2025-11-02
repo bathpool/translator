4245592b5314878b1abe22022d02a9960545e26a
bathpool <gujie2000@hotmail.com>
2025-11-01 18:56:44 -0700

    v28

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.static('public'))
 app.use(express.json());
 app.use('/api', apiRoutes)
 
+app.get('/', (req, res) => {
+    // If index.html exists in public/, Vercel serves that instead
+    res.send('Welcome to the API server'); 
+});
+
 // app.listen(PORT, ()=> console.log(`server connected on port ${PORT}`))
 
 export default app
